5a0b53cf0b4c18eeb8a1c65dccef952dd0c00209
qtungnui07 <qtung.07.dt@gmail.com>
2025-12-31 11:10:06 +0700

    midterm fix1

--- midterm/10.py
+++ midterm/10.py
@@ -1,14 +1,14 @@
-n=int(input())
-arr=list(map(int,input().split()))
-if len(arr)==n:
-    for i in range(len(arr)):
-        idx=i
-        for j in range(i+1,len(arr)):
-            if arr[j]>arr[idx]:
-                idx=j
-                arr[i],arr[idx]=arr[idx],arr[i]
-                print(arr)
+n = int(input())
+arr = list(map(int, input().split()))
+if len(arr) == n:
+    for i in range(n):
+        idx = i
+        print(arr)
+        for j in range(i + 1, n):
+            if arr[j] > arr[idx]:
+                idx = j
+        arr[i], arr[idx] = arr[idx], arr[i]
+
     print(*arr)
-    
 else:
     print("chua viet du so phan tu, viet thua so phan tu")
--- midterm/4.py
+++ midterm/4.py
@@ -1,5 +1,6 @@
-a, b=map(int,input().split())
-n=int(input("Chọn \n 1 để nhân 2 số \n 2 để chia 2 số \n 3 để cộng 2 số \n 4 để trừ 2 số \n 5 in ra số bé hơn trong 2 số đó \n "))
+a, b=map(int,input("nhap 2 so nguyen duoc cach boi dau cach: ").split())
+print("Chọn \n 1 để nhân 2 số \n 2 để chia 2 số \n 3 để cộng 2 số \n 4 để trừ 2 số \n 5 in ra số bé hơn trong 2 số đó ")
+n=int(input())
 if n==1:print(a*b)
 elif n==2:print(a/b)
 elif n==3:print(a+b)

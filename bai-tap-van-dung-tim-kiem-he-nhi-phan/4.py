c48eabed2de073baa27548561fd1bf2200eaef7f
qtungnui07 <qtung.07.dt@gmail.com>
2026-01-05 11:49:36 +0700

    finish bai tap van dung tim kiem nhi phan

--- bai-tap-van-dung-tim-kiem-he-nhi-phan/4.py
+++ bai-tap-van-dung-tim-kiem-he-nhi-phan/4.py
@@ -15,15 +15,3 @@ class Logger:
 log_file = __file__.replace(".py", ".txt")
 sys.stdout = Logger(log_file)
 sys.stderr = sys.stdout
-n=int(input("n="))
-a=list(map(int,input("nhap mang: ").split()))
-for i in range(1,len(a)):
-    key=a[i]
-    j=i-1
-    while j>=0 and key>a[j]:
-        a[j+1]=a[j]
-        j-=1
-    a[j+1]=key
-print(a)
-x=int(input("x="))
-print(bs_desc(a,x))
